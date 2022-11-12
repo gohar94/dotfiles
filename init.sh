8a9e3d1b1ac9483ec0f9f4eaa3b9a137284dea79
Gohar Irfan Chaudhry <girfan@mit.edu>
2022-11-12 14:28:47 -0500

    init.sh: vimrc symlink fix

--- init.sh
+++ init.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -xe
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Globals
+SCRIPT_DIR=$(dirname $(readlink -f $0))
 
+# === vim ===
+## Download plugins for vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+## Backup previous vimrc
 mv ~/.vimrc ~/.vimrc.old
-ln -s ./vimrc ~/.vimrc
+## Link new vimrc
+ln -s $SCRIPT_DIR/vimrc ~/.vimrc
+## Post-install instructions
+echo "Run :PluginInstall after opening vim for the first time"
