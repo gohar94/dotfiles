060182d6edce035fbceede1722491bb45e4490d0
Gohar Irfan Chaudhry <girfan@mit.edu>
2022-11-13 12:52:46 -0500

    init.sh: tmux.conf

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -xe
+set -x
 
 # Globals
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
-# === vim ===
+# ==================================== vim ====================================
 ## Download plugins for vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ## Backup previous vimrc
@@ -13,3 +13,9 @@ mv ~/.vimrc ~/.vimrc.old
 ln -s $SCRIPT_DIR/vimrc ~/.vimrc
 ## Post-install instructions
 echo "Run :PluginInstall after opening vim for the first time"
+
+# ==================================== tmux ====================================
+## Backup previous tmux.conf
+mv ~/.tmux.conf ~/.tmux.conf.old
+## Link new tmux.conf
+ln -s $SCRIPT_DIR/tmux.conf ~/.tmux.conf
