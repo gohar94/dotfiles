c5a00db6ae3d2804831e3d326a301ba0ae9bd598
Gohar Irfan Chaudhry <girfan@mit.edu>
2023-11-04 13:38:36 -0400

    init_git: bugfix

--- init_git.sh
+++ init_git.sh
@@ -6,6 +6,6 @@ SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 # ==================================== git ====================================
 git config --global user.name "Gohar Irfan Chaudhry"
-git config --global user.name "girfan@mit.edu"
+git config --global user.email "girfan@mit.edu"
 git config --global core.editor "vim"
 git config pull.rebase false
